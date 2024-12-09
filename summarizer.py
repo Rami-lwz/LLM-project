3bbf1fd21ec5ba7dd723fd2d034febd48eddcc71
Sacha Guenoun <75856103+Sacha-Lunae@users.noreply.github.com>
2024-12-09 21:29:19 +0100

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,5 +1,6 @@
 from transformers import pipeline
-import openai
+from openai import OpenAI
+
 def resume(text,max_length,min_length):
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
     return summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)
@@ -9,20 +10,25 @@ def resume_formated(text,max_length,min_length,formater,apikey=None):
     summary = summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)
     return formater(summary[0]['summary'],apikey)
 
-def format_summary_openai(summary_text, openai_api_key=None):
-    openai.api_key = openai_api_key
-    
-    # Prompt for formatting the summary into structured notes
-    prompt = (
-        "Format the following text into structured study notes with headings, subheadings, and bullet points:\n\n"
-        f"{summary_text}\n\n"
-        "Output:\n- Main Heading\n  - Subheading 1\n    - Key Point 1\n    - Key Point 2\n  - Subheading 2\n    - Key Point 3\n"
-    )
-    
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=len(summary_text),
-        temperature=0.7
-    )
-    return response["choices"][0]["text"].strip()
+
+def format_summary_openai(summary, api_key="sk-proj-KEY", model="gpt-4o-mini"):
+        
+        client = OpenAI(api_key=api_key)
+
+        system_prompt= """
+            "Format the following text into structured study notes with headings, subheadings, and bullet points. Do NOT add more info than what there is already. Make it look like a real summary of a course.
+            "Output:\n- Main Heading\n - Little sentence\n - Subheading 1\n    - Key Point 1\n    - Key Point 2\n  - Subheading 2\n    - Key Point 3\n"
+    """
+        messages = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": summary}
+        ]
+        
+        chat_completion = client.chat.completions.create(
+            messages=messages,
+            model=model,
+            temperature = 0.05
+        )
+
+        message_content = chat_completion.choices[0].message.content
+        return(message_content)
