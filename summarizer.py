7bfc805902022683ef3d2a150f3688c9a2787f5d
NoeBrt <noe.breton@efrei.net>
2024-12-09 20:31:04 +0100

    add format summary

--- summarizer.py
+++ summarizer.py
@@ -3,3 +3,7 @@ from transformers import pipeline
 def resume(text,max_length,min_length):
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
     return summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)
+
+def format_summary(summary):
+    #format the summary text to structured notes
+    pass
